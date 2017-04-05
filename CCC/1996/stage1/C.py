81569953f3c3f1a05ade434b654df737ff214e9c
ousmane.amadou <ousmane.amadou89@gmail.com>
2017-04-05 16:20:56 -0400

    Changes to 1996 C.py

--- CCC/1996/stage1/C.py
+++ CCC/1996/stage1/C.py
@@ -2,14 +2,36 @@
     Link to problem description:
         http://cemc.uwaterloo.ca/contests/computing/1996/stage1/c-prob.html
 
-    0*2^0 + 2^1 + 2^2 ... + 2^n
+    Solution:
+    1. Let n be the length of the bit string.
+    2. Let k bt the number of ones in the bit string.
+    3. Find the initial bitstring with k ones followed by n-k zeroes.
+    4. Left shift all the bits with the exception of the first bit.
+    5.
+
+    Consider n=6, k=3.
+    1 11000
+
+    1 01100
+    1 00110
+    1 00011
+
+    11 0110
+    11 0011
+
+    111 000
 """
 
-num_cases = int(input())
-pairs = []
-for case in range(num_cases):
-    i, j = map(int, input().split())
-    pairs.append([i, j])
+nc = int(input())           # Read in number of cases
+cases = [[0, 0]*nc]         # A queue to hold the cases we want to deal with
 
-for pair in pairs:
-    n, k = pair
+for i in range(nc):
+    cases[i][0], cases[i][1] = map(int, input().split())
+
+for case in cases:
+    ibp = 0
+    n, k = case
+    for i in range(k):
+        ibp += 2**(n-i)
+
+    print(ibp)
