389e0f97dfcec095d0f86b843382dd4a55dbe82b
ousmane.amadou <ousmane.amadou89@gmail.com>
2017-04-06 01:52:16 -0400

    Sooooo big.

--- CCC/1996/stage1/D.py
+++ CCC/1996/stage1/D.py
@@ -45,7 +45,78 @@ def converttoint(numeral):
 
 
 def converttonumeral(i):
-    
+    """ (int) -> String
+        Takes an integer and converts it into a roman numeral.
+    """
+    numeral = ""
+    if i > 1000:
+        numeral = "CONCORDIA CUM VERITATE"
+    else:
+        digits = list(str(i))
+        digits = ['-1']*(4-len(digits)) + digits
+
+        for j in range(4):
+            if not digits[j] == '-1':
+                if j == 0:
+                    numeral = "M"
+                elif j == 1:
+                    if digits[j] == "1":
+                        numeral += 'C'
+                    elif digits[j] == "2":
+                        numeral += 'CC'
+                    elif digits[j] == "3":
+                        numeral += 'CCC'
+                    elif digits[j] == "4":
+                        numeral += 'CD'
+                    elif digits[j] == "5":
+                        numeral += 'D'
+                    elif digits[j] == "6":
+                        numeral += 'DC'
+                    elif digits[j] == "7":
+                        numeral += 'DCC'
+                    elif digits[j] == "8":
+                        numeral += 'DCCC'
+                    elif digits[j] == "9":
+                        numeral += 'CM'
+                elif j == 2:
+                    if digits[j] == "1":
+                        numeral += 'X'
+                    elif digits[j] == "2":
+                        numeral += 'XX'
+                    elif digits[j] == "3":
+                        numeral += 'XXX'
+                    elif digits[j] == "4":
+                        numeral += 'XL'
+                    elif digits[j] == "5":
+                        numeral += 'L'
+                    elif digits[j] == "6":
+                        numeral += 'LX'
+                    elif digits[j] == "7":
+                        numeral += 'LXX'
+                    elif digits[j] == "8":
+                        numeral += 'LXXX'
+                    elif digits[j] == "9":
+                        numeral += 'C'
+                elif j == 3:
+                    if digits[j] == "1":
+                        numeral += 'I'
+                    elif digits[j] == "2":
+                        numeral += 'II'
+                    elif digits[j] == "3":
+                        numeral += 'III'
+                    elif digits[j] == "4":
+                        numeral += 'IV'
+                    elif digits[j] == "5":
+                        numeral += 'V'
+                    elif digits[j] == "6":
+                        numeral += 'VI'
+                    elif digits[j] == "7":
+                        numeral += 'VII'
+                    elif digits[j] == "8":
+                        numeral += 'VIII'
+                    elif digits[j] == "9":
+                        numeral += 'IX'
+    return numeral
 
 for case in cases:
     fnum, snum = case.split('+')
@@ -55,4 +126,4 @@ for case in cases:
     total += converttoint(fnum)
     total += converttoint(snum)
 
-    print(total)
+    print(case + converttonumeral(total))
