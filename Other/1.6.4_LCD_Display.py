a2a67be77729f5e4548b7d0e2b75b1f5db8f49ad
ousmane.amadou <ousmane.amadou89@gmail.com>
2017-04-08 15:42:32 -0400

    Almost Complete 1.6.4

--- Other/1.6.4_LCD_Display.py
+++ Other/1.6.4_LCD_Display.py
@@ -12,9 +12,41 @@ while True:
         break
     output = ""
     for i in n:
-        for j in range(1, (s+2) + 1):
-            if j == 1:  # Deals with top comment
-                output += " " + nums[i][0]*s + " "
-            elif j > 1 and j < 1 + s:
-                #
-            else:
+        for j in range(0, (2s+3)):
+            if j == 0:  # Deals with quad 0
+                if nums[i][0]:
+                    output += " " + "-"*s + " "
+                else:
+                    output += " " + " "*s + " "
+            elif j > 0 and j < 1 + s:       # Deals with 1st and 2nd quads
+                if num[i][1]:
+                    output += "|"
+                else:
+                    output += " "
+                output = " "*s
+                if num[i][2]:
+                    output += "|"
+                else:
+                    output += " "
+            elif j == 1 + s:                # Deals with the third quad
+                if nums[i][3]:
+                    output += " " + "-"*s + " "
+                else:
+                    output += " " + " "*s + " "
+            elif j > 1 + s and j < 2s + 2:  # Deals with the 4th and 6th quads
+                if num[i][4]:
+                    output += "|"
+                else:
+                    output += " "
+                output = " "*s
+                if num[i][6]:
+                    output += "|"
+                else:
+                    output += " "
+            else:                           # Deals with the 5th
+                if nums[i][5]:
+                    output += " " + "-"*s + " "
+                else:
+                    output += " " + " "*s + " "
+
+print(output)
