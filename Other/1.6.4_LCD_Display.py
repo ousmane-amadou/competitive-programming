2a4f0d0b65bd64c610b847378cb0a7065a2785d3
ousmane.amadou <ousmane.amadou89@gmail.com>
2017-04-08 16:09:04 -0400

    Final changes to 1.6.4

--- Other/1.6.4_LCD_Display.py
+++ Other/1.6.4_LCD_Display.py
@@ -1,8 +1,8 @@
-nums = {0: [1, 1, 1, 0, 1, 1, 1], 1: [0, 0, 1, 0, 0, 0, 1],
-        2: [1, 0, 1, 1, 1, 1, 0], 3: [1, 0, 1, 1, 0, 1, 1],
-        4: [0, 1, 1, 1, 0, 0, 1], 5: [1, 1, 0, 1, 0, 1, 1],
-        6: [1, 1, 0, 1, 1, 1, 1], 7: [1, 0, 1, 0, 0, 1, 0],
-        8: [1, 1, 1, 1, 1, 1, 1], 9: [1, 1, 1, 1, 0, 1, 1]}
+nums = {'0': [1, 1, 1, 0, 1, 1, 1], '1': [0, 1, 0, 0, 1, 0, 0],
+        '2': [1, 0, 1, 1, 1, 1, 0], '3': [1, 0, 1, 1, 0, 1, 1],
+        '4': [0, 1, 1, 1, 0, 0, 1], '5': [1, 1, 0, 1, 0, 1, 1],
+        '6': [1, 1, 0, 1, 1, 1, 1], '7': [1, 0, 1, 0, 0, 0, 1],
+        '8': [1, 1, 1, 1, 1, 1, 1], '9': [1, 1, 1, 1, 0, 1, 1]}
 
 while True:
     s, n = input().split()
@@ -11,20 +11,20 @@ while True:
     if s == 0 and n == "0":
         break
     output = ""
-    for i in n:
-        for j in range(0, (2s+3)):
+    for j in range(2*s+3):
+        for i in n:
             if j == 0:  # Deals with quad 0
                 if nums[i][0]:
                     output += " " + "-"*s + " "
                 else:
                     output += " " + " "*s + " "
             elif j > 0 and j < 1 + s:       # Deals with 1st and 2nd quads
-                if num[i][1]:
+                if nums[i][1]:
                     output += "|"
                 else:
                     output += " "
-                output = " "*s
-                if num[i][2]:
+                output += " "*s
+                if nums[i][2]:
                     output += "|"
                 else:
                     output += " "
@@ -33,20 +33,21 @@ while True:
                     output += " " + "-"*s + " "
                 else:
                     output += " " + " "*s + " "
-            elif j > 1 + s and j < 2s + 2:  # Deals with the 4th and 6th quads
-                if num[i][4]:
+            elif j > 1 + s and j < 2*s + 2:  # Deals with the 4th and 6th quads
+                if nums[i][4]:
                     output += "|"
                 else:
                     output += " "
-                output = " "*s
-                if num[i][6]:
+                output += " "*s
+                if nums[i][6]:
                     output += "|"
                 else:
                     output += " "
-            else:                           # Deals with the 5th
+            else:                           # Deals with the 5th quads
                 if nums[i][5]:
                     output += " " + "-"*s + " "
                 else:
                     output += " " + " "*s + " "
+        output += '\n'
 
 print(output)
