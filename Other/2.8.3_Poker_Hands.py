fd3bcd97e055bb4c6e92456a526c203e54f55066
ousmane.amadou <ousmane.amadou89@gmail.com>
2017-04-09 17:13:50 -0400

    I might just say how i feel

--- Other/2.8.3_Poker_Hands.py
+++ Other/2.8.3_Poker_Hands.py
@@ -10,56 +10,90 @@ def vald(a):
     return VALS[a]
 
 
-def isSF(h):
-    return h[0] == h[1] == h[2] == h[3] == h[4]
+def isSF(h):  # OK
+    r = [-1, -1]
+    if h[0] == h[1] == h[2] == h[3] == h[4]:
+        r = [10, h[4][0]]
+    return r
 
 
-def isFK(h):
+def isFK(h):  # OK
     sorted(vald, key=vald)
-    bf1 = h[0][0] == h[1][0] == h[2][0] == h[3][0]
-    bf2 = h[1][0] == h[2][0] == h[3][0] == h[4][0]
-    return bf1 or bf2
+    r = [-1, -1]
+    if h[0][0] == h[1][0] == h[2][0] == h[3][0]:
+        r = [9, h[3][0]]
+    elif [1][0] == h[2][0] == h[3][0] == h[4][0]:
+        r = [9, h[3][0]]
+    return r
 
 
 def isFH(h):
     sorted(vald, key=vald)
-    bf1 = (h[0][0] == h[1][0] == h[2][0]) and (h[3][0] == h[4][0])
-    bf2 = (h[1][0] == h[2][0]) and (h[3][0] == h[4][0] == h[5][0])
-    return bf1 or bf2
+    r = [-1, -1]
+    if (h[0][0] == h[1][0] == h[2][0]) and (h[3][0] == h[4][0]):
+        r = [8, h[2][0]]
+    elif (h[1][0] == h[2][0]) and (h[3][0] == h[4][0] == h[5][0]):
+        r = [8, h[3][0]]
+    return r
 
 
 def isFL(h):
-    return h[0][1] == h[1][1] == h[2][1] == h[3][1] == h[4][1]
+    sorted(vald, key=vald)
+    r = [-1, -1]
+    if h[0][1] == h[1][1] == h[2][1] == h[3][1] == h[4][1]:
+        r = [7, h[4][1]]
+    return r
 
 
 def isST(h):
     sorted(vald, key=vald)
-    bf1 = VALS[h[0][0]] == (VALS[h[1][0]] + 1)
-    bf2 = VALS[h[1][0]] == (VALS[h[2][0]] + 1)
-    bf3 = VALS[h[2][0]] == (VALS[h[3][0]] + 1)
-    bf4 = VALS[h[3][0]] == (VALS[h[4][0]] + 1)
-    return bf1 and bf2 and bf3 and bf4
+    r = [-1, -1]
+    bf1 = VALS[h[1][0]] == (VALS[h[0][0]] + 1)
+    bf2 = VALS[h[2][0]] == (VALS[h[1][0]] + 1)
+    bf3 = VALS[h[3][0]] == (VALS[h[2][0]] + 1)
+    bf4 = VALS[h[4][0]] == (VALS[h[3][0]] + 1)
+    if bf1 and bf2 and bf3 and bf4:
+        r = [6, h[4][0]]
+
+    return [-1, -1]
 
 
 def isTOK(h):
     sorted(vald, key=vald)
+    r = [-1, -1]
     bf1 = (h[0][0] == h[1][0] == h[2][0])
     bf2 = (h[1][0] == h[2][0] == h[3][0])
     bf3 = (h[2][0] == h[3][0] == h[4][0])
-    return bf1 or bf2 or bf3
-
-
-def isP(h):
-    return "penis"
+    if bf1 or bf2 or bf3:
+        r = [5, h[2][0]]
 
 
 def is2P(h):
+    sorted(vald, key=vald)
+    r = [-1, -1]
+    if h[0][0] == h[1][0] and h[1][0] == h[2][0]:
+        r = [4, h[1][0]]
+    if h[2][0] == h[3][0] and h[3][0] == h[4][0]:
+        r = [4, max(r[1], h[3][0])]
+    return r
+
+
+def isP(h):
+    sorted(vald, key=vald)
+    r = [-1, -1]
+    if h[0][0] == h[1][0] or h[1][0] == h[2][0]:
+        r = [3, h[1][0]]
+    elif h[2][0] == h[3][0] or h[3][0] == h[4][0]:
+        r = [3, h[3][0]]
+    return r
+
+
+def isHC(h):
     sorted(vald, key=vald)
     bf1 = (h[0][0] == h[1][0] == h[2][0])
     bf2 = (h[1][0] == h[2][0] == h[3][0])
     bf3 = (h[2][0] == h[3][0] == h[4][0])
     return bf1 or bf2 or bf3
-
 # NO NEED TO TEST FOR HIGH CARD
 
 while True:
