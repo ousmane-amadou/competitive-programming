aca31c5189c060830b9165abd5580e56d8d1a606
ousmane.amadou <ousmane.amadou89@gmail.com>
2017-04-09 17:27:01 -0400

    2.8.3 Donzoo

--- Other/2.8.3_Poker_Hands.py
+++ Other/2.8.3_Poker_Hands.py
@@ -10,14 +10,14 @@ def vald(a):
     return VALS[a]
 
 
-def isSF(h):  # OK
+def isSF(h):
     r = [-1, -1]
     if h[0] == h[1] == h[2] == h[3] == h[4]:
         r = [10, h[4][0]]
     return r
 
 
-def isFK(h):  # OK
+def isFK(h):
     sorted(vald, key=vald)
     r = [-1, -1]
     if h[0][0] == h[1][0] == h[2][0] == h[3][0]:
@@ -90,11 +90,7 @@ def isP(h):
 
 def isHC(h):
     sorted(vald, key=vald)
-    bf1 = (h[0][0] == h[1][0] == h[2][0])
-    bf2 = (h[1][0] == h[2][0] == h[3][0])
-    bf3 = (h[2][0] == h[3][0] == h[4][0])
-    return bf1 or bf2 or bf3
-# NO NEED TO TEST FOR HIGH CARD
+    return [2, h[4][0]]
 
 while True:
     cards = input().split(' ')
@@ -108,33 +104,55 @@ while True:
     p2r = [-1, -1]
 
     i = 10
-    win = [0, 0]
-    while (p1r == [-1, -1]) or (p2r == [-1, -1]):
-        if i == 10:
-            if ifSF(p1h):
-                p1r = [10, cards[0][0]]
-                win[0] = 1
-            if ifSF(p2h):
-                p1r = [10, cards[0][0]]
-                win[1] = 1
-        elif i == 9:
-            if isFK(p1h):
-                p1r = [10, cards[3][0]]
-                win[0] = 1
-            if isFK(p2h):
-                p1r = [10, cards[3][0]]
-                win[1] = 1
-        elif i == 8:
-            if isFH(p1h):
-                p1r = [10, cards[3][0]]
-                win[0] = 1
-            if isFH(p2h):
-                p1r = [10, cards[3][0]]
-                win[1] = 1
+    winner = -1
+    while winner == -1 or i < 2:
+        if i == 2:
+            p1r = isHC(p1h)
+            p2r = isHC(p2h)
+        elif i == 3:
+            p1r = isP(p1h)
+            p2r = isP(p2h)
+        elif i == 4:
+            p1r = is2P(p1h)
+            p2r = is2P(p2h)
+        elif i == 5:
+            p1r = isTOK(p1h)
+            p2r = isTOK(p2h)
+        elif i == 6:
+            p1r = isST(p1h)
+            p2r = isST(p2h)
         elif i == 7:
-            if isFH(p1h):
-                p1r = [10, cards[3][0]]
-                win[0] = 1
-            if isFH(p2h):
-                p1r = [10, cards[3][0]]
-                win[1] = 1
+            p1r = isFL(p1h)
+            p2r = isFL(p2h)
+        elif i == 8:
+            p1r = isFH(p1h)
+            p2r = isFH(p2h)
+        elif i == 9:
+            p1r = isFK(p1h)
+            p2r = isFK(p2h)
+        elif i == 10:
+            p1r = isSF(p1h)
+            p2r = isSF(p2h)
+        i -= 1
+        # Handle Aftermath of looking at hands
+        if p1r[0] == -1 and p2r[0] == -1:
+            pass
+        elif p1r[0] == -1:
+            winner = 2
+            break
+        elif p2r[0] == -1:
+            winner = 1
+            break
+        else:
+            if p1r[1] == p2r[1]:
+                pass
+            elif p1r[1] > p2r[1]:
+                winner = 1
+            else:
+                winner = 2
+    if winner == -1:
+        print 'Tie.'
+    elif winner == 1:
+        print 'Black Wins.'
+    else:
+        print 'White Wins.'
