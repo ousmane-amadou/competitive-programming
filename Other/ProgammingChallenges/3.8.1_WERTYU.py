c4fab928f7eed408353d40976f1201cbf9c2a142
ousmane.amadou <ousmane.amadou89@gmail.com>
2017-05-21 14:47:23 -0400

    Directory Restructuring

---
+++ Other/ProgammingChallenges/3.8.1_WERTYU.py
@@ -0,0 +1,10 @@
+key_map = { 'W':'Q', 'E':'W', 'R':'E', 'T':'R', 'Y':'T', 'U':'Y', 
+		'I':'U', 'O':'I', 'P':'O', '[':'P', ']':'[', '\\':']',
+		'S':'A', 'D':'S', 'F':'D', 'G':'F', 'H':'G', 'J':'H',
+		'K':'J', 'L':'K', ';':'L', 'X':'Z', 'C':'X', 'V':'C',
+		'B':'V', 'N':'B', 'M':'N', ',':'M', '.':',' }
+
+# YOU STILL NEED TO PROGRAM IN THE ESCAPE SEQUENCES
+# FIND OUT HOW TO DO FILE I/O OR WORK WITH CONTESTS INPUT
+
+while(
