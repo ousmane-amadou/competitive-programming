6587b27557a0a93ea1ed5090f4a6ea8d860c1d19
ousmane-amadou <ousmane.amadou@mail.utoronto.ca>
2017-11-16 23:59:32 -0500

    decked out atom, restructred more directories, ready to go.

---
+++ solutions/ccc/2015/junior/specialday.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+  int d, m;
+  cin >> d >> m;
+
+  if (m == 2) {
+
+  } else if (m < 2) {
+  }
+  if (m < 2) {
+  }
+}
--- solutions/codeforces/282/A/main.cpp
+++ solutions/codeforces/282/A/main.cpp
@@ -3,26 +3,25 @@
 #include <vector>
 
 /* Topics
- * Basic I/0, Namespaces 
+ * Basic I/0, Namespaces
  * vectors, string [stl]
  */
-
 
 using namespace std;
 int n;
 vector<string> statements;
 
 int main() {
-	int X = 0;
-	cin >> n;
-	for(int i=0; i<n; i++) {
-		string s; 
-		cin >> s;	
-		if (s.find("++") == string::npos) {
-			X -= 1;
-		} else {
-			X += 1;
-		}
-	}
-	cout << X;
+  int X = 0;
+  cin >> n;
+  for (int i = 0; i < n; i++) {
+    string s;
+    cin >> s;
+    if (s.find("++") == string::npos) {
+      X -= 1;
+    } else {
+      X += 1;
+    }
+  }
+  cout << X;
 }
