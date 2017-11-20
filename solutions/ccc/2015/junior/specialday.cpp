0a64c15fc6e4d96db8bb06779a75f8198927ca0d
ousmane-amadou <ousmane.amadou@mail.utoronto.ca>
2017-11-20 17:20:26 -0500

    finished ccc2015 j1

--- solutions/ccc/2015/junior/specialday.cpp
+++ solutions/ccc/2015/junior/specialday.cpp
@@ -5,12 +5,19 @@ using namespace std;
 
 int main() {
   int d, m;
-  cin >> d >> m;
+  cin >> m >> d;
 
   if (m == 2) {
-
+    if (d == 18) {
+      cout << "Special";
+    } else if (d < 18 ){
+      cout << "Before";
+    } else {
+      cout << "After";
+    }
   } else if (m < 2) {
-  }
-  if (m < 2) {
+    cout << "Before";
+  } else {
+    cout << "After";
   }
 }
