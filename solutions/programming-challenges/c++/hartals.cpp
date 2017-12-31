fe9b2c5098d80ce53e4b295ce598e8ec96b9374a
ousmane-amadou <ousmane.amadou@mail.utoronto.ca>
2017-12-30 22:54:03 -0500

    Some directory restructuring, modified some data structures, now committing

---
+++ solutions/programming-challenges/c++/hartals.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void process();
+
+int main() {
+	int T, N, P;
+	vector<int> parties;
+
+	cin >> T;
+	for(int i = 0; i < T; i++) {
+		cin >> N; cin >> P;
+		for(int j = 0; j < P; j++) {
+			// To be continued
+		}
+	}
+}
