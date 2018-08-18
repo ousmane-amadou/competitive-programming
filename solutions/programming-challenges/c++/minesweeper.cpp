9236f4ccce4b4e0955bbac17140623d7ff1c4eb1
ousmane-amadou <ousmane.amadou@mail.utoronto.ca>
2018-08-17 22:31:26 -0400

    adj_mines function has been added with description comments. Variables have been allocated. Skeleton of main algorithm filled in.

--- solutions/programming-challenges/c++/minesweeper.cpp
+++ solutions/programming-challenges/c++/minesweeper.cpp
@@ -1,12 +1,34 @@
 #include <iostream>
 using namespace std;
 
-int n, m;
+const int MINE = -1;
+
+/* Calculate the number of mines adjacent to the specified
+square.
+
+Precondition (s):
+(1) mine_field != null
+(2) 0 < square[0] <= size(mine_field)
+(3) 0 < square[1] <= size(mine_field[0])
+*/
+int adj_mines(int mine_field[][], int square[]) {
+
+}
+
+int n, m;         // Represents the number of rows and columns in input matrix
+int mine_field[100][100];       // Represents the input matrix
+int adjcancey_field[100][100];      // Represents the output matrix
 int main() {
+
   scanf("%d %d", &n, &m);
+
   for (int i = 0; i < n; i++) {
     for (int j = 0; j < n; j++) {
-
+      if (mine_field[i][j] != MINE) {
+        adjcancey_field[i][j] = adj_mines(mine_field, {i, j});
+      } else {
+        adjcancey_field[i][j] = MINE;
+      }
     }
   }
 }
