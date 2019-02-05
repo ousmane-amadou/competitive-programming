fbd71cd584ea171f0bfab9d512ae8bf4c23d7604
ousmane-amadou <amadouou@teach.cs.toronto.edu>
2019-02-05 17:36:17 -0500

    Bare bones code including method declarations and input processing have been laid out for the Crypt Kicker Problem

---
+++ solutions/programming-challenges/c++/crypt_kicker.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+final const int MAX_SIZE;
+
+/* Precondition:
+ * 	- dict & words both sorted by word length (and other things
+ */ 
+void decrypt(string* dict, string* words);
+
+int main() {
+	int n;
+	string words[MAX_SIZE];
+	
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		
+	
+	}
+
+	cout << "Hello World!" << endl;
+}
+
+
