e66c16c67d22c06d1c9283958357d5b8fdc68e37
ousmane-amadou <amadouou@teach.cs.toronto.edu>
2019-02-06 15:58:29 -0500

    Solved question.. Commencing implementation of component functions

--- solutions/programming-challenges/c++/crypt_kicker.cpp
+++ solutions/programming-challenges/c++/crypt_kicker.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 #include <string>
+#include <map>
+#include <vector>
 
 using namespace std;
 
 final const int MAX_SIZE;
 
-/* Precondition:
- * 	- dict & words both sorted by word length (and other things
- */ 
+set<int> get_possible_soluions(char ch, set<string> dictionary, set<string> enc_words); 
+bool update_solution(map<int, vector<int>> solution, char ch, set<int> poss_solutions);
 void decrypt(string* dict, string* words);
 
 int main() {
@@ -22,6 +23,10 @@ int main() {
 	}
 
 	cout << "Hello World!" << endl;
+}
+
+set<int> get_possible_solutions(char ch, set<string> dictionary, set<string> enc_words) {
+	set<int> poss_solutions;
 }
 
 
