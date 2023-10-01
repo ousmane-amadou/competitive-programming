40712e9333f82abe316d1fda083a0ba33ad51b9c
unknown <ousmane.amadou89@gmail.com>
2023-10-01 10:48:56 -0400

    Added a link to Problem 266.A on codeforces, and a possible implementation

--- solutions/codeforces/js/stonesontable.js
+++ solutions/codeforces/js/stonesontable.js
@@ -1,6 +1,18 @@
+/* 
+ * Link to problem: https://codeforces.com/problemset/problem/266/A
+ */
 var stones = readline()
 
-for(var i; i < stones.length-1; i++){
-    if(stones.charAt(i)==stones.charAt(i+1))
-        stones
+function deleteCharAt(str, i) {
+    return str.substring(0, i+1) + str.slice(i+2)
 }
+
+var sol = 0;
+for(var i; i < stones.length-1; i++){
+    if(stones.charAt(i) == stones.charAt(i+1)) {
+        stones = deleteCharAt(stones, i)
+        i--; sol++;
+    }
+        
+}
+
