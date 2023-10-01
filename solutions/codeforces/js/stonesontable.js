5ef5e07a1fbcc85042c802aff04c517edef6a659
unknown <ousmane.amadou89@gmail.com>
2023-10-01 11:59:56 -0400

    Added a check to see if all stones were unique, removed supposed base case; solution accepted

--- solutions/codeforces/js/stonesontable.js
+++ solutions/codeforces/js/stonesontable.js
@@ -4,15 +4,22 @@
 var stone_length = Number(readline())
 var stones = readline()
 
+function allUnique(str) {
+    for(var i = 0; i < str.length; i++){
+        if(stones.charAt(i) == stones.charAt(i+1)) {
+            return false;
+        }   
+    }
+    return true;
+}
 function deleteCharAt(str, i) {
     return str.substring(0, i+1) + str.slice(i+2)
 }
 
 var sol = 0;
 
-
 for(var i = 0; i < stone_length-1; i++) {
-    if(stones.length == 1) {
+    if(allUnique(stones)) {
         break
     }
     if(stones.charAt(i) == stones.charAt(i+1)) {
